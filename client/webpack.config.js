86ef1b2ebc67c3761b645927592413df049a6156
Ehte22 <syedehteshamali22@gmail.com>
2024-12-26 15:36:59 +0530

    update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = (_, argv) => ({
     publicPath: "https://ecom-cart-app-client.vercel.app/",
   },
 
+
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
   },
